c89fa2e8c373ceda7426f00099598822b9d9b7b9
Mathilde Ballouhey <mathildeballouhey.nausikaa44@gmail.com>
2023-09-27 10:03:34 +0200

    add: Init repository

---
+++ src/views/LogView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex flex-wrap justify-content-center max-h-screen">
+        <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6 align-self-center">
+            <div class="text-center mb-5">
+                <img
+                    src="images/blocks/logos/hyper.svg"
+                    alt="Image"
+                    height="50"
+                    class="mb-3"
+                />
+                <div class="text-900 text-3xl font-medium mb-3">
+                    Bienvenue
+                </div>
+            </div>
+
+            <div>
+                <label for="email1" class="block text-900 font-medium mb-2"
+                    >Email</label
+                >
+                <InputText id="email1" type="text" class="w-full mb-3" />
+
+                <label for="password1" class="block text-900 font-medium mb-2"
+                    >Mot de passe</label
+                >
+                <InputText id="password1" type="password" class="w-full mb-3" />
+
+                <Button
+                    label="Se connecter"
+                    icon="pi pi-user"
+                    class="w-full"
+                ></Button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
