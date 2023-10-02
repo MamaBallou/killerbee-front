6e471c85726ef036406ebde2da89f3915866d458
Mathilde Ballouhey <mathildeballouhey.nausikaa44@gmail.com>
2023-10-02 16:17:11 +0200

    add: Authentication service and login

---
+++ src/service/AuthService.ts
@@ -0,0 +1,22 @@
+import axios, { type AxiosResponse } from "axios";
+
+type auth = {
+    Email: string;
+    Password: string;
+};
+
+export const AuthService = {
+    login(payload: auth) {
+        return axios.post("/", payload);
+    },
+    verifyToken(token: string) {
+        return axios.get("/", {
+            headers: {
+                Authorization: token,
+            },
+            params: {
+                Table: "Modèle"
+            }
+        });
+    },
+};
---
+++ src/stores/auth.ts
@@ -0,0 +1,49 @@
+import { AuthService } from "@/service/AuthService";
+import { ModelService } from "@/service/ModelService";
+import { Axios, type AxiosResponse } from "axios";
+import { defineStore } from "pinia";
+
+interface State {
+    Authenticated: Promise<AxiosResponse>;
+    token: string | null;
+}
+
+export const useAuthStore = defineStore({
+    id: "auth",
+    state: (): State => ({
+        Authenticated: Promise.resolve({} as AxiosResponse),
+        token: null,
+    }),
+    getters: {
+        isAuthenticated: (state: State) => {
+            return AuthService.verifyToken(localStorage.getItem("token") ?? "")
+                .then((response: AxiosResponse) => {
+                    if (response.status != 200) {
+                        localStorage.removeItem("token");
+                    }
+                })
+                .catch((error) => {
+                    localStorage.removeItem("token");
+                });
+        },
+        getToken: (state: State) => {
+            return localStorage.getItem("token") ?? null;
+        },
+    },
+    actions: {
+        login(payload: { Email: string; Password: string }) {
+            return AuthService.login(payload).then(
+                (response: AxiosResponse) => {
+                    if (response.status == 200) {
+                        this.token = response.data.token;
+                        localStorage.setItem("token", response.data.token);
+                    }
+                }
+            );
+        },
+        logout() {
+            this.token = null;
+            localStorage.removeItem("token");
+        },
+    },
+});
